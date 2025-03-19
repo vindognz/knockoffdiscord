7c1d93b2da1cd7c12c379d5c38812e6b555696ba
Vincent Rodley <vindognz@gmail.com>
2025-03-19 21:55:22 +1300

    added hashing of passwords

--- index.php
+++ index.php
@@ -27,17 +27,21 @@ if (isset($_POST["login"])) { // checks if you loaded the page by pressing the s
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             // gets the password that corresponds to the inputted username
-            $stmt = $pdo->prepare("SELECT password FROM users WHERE username = :username");
+            $stmt = $pdo->prepare("SELECT password, salt FROM users WHERE username = :username");
             $stmt->execute([':username' => $username]);
-            $dbPassword = $stmt->fetchColumn();
+
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $dbPassword = $data[0]['password'];
+            $dbSalt = $data[0]['salt'];
 
             // if the correct password in the db is the same as the inputted one:
-            if ($dbPassword == $password) {
+            if ($dbPassword == hash('sha256', $dbSalt.$password)) {
                 // successful login!
                 $_SESSION['loggedIn'] = true;
                 $_SESSION['username'] = $username;
 
-                header('location: '.'/');
+                header('location: '.'/login');
             } else {
                 // unsuccessful login.
                 ?> <script>alert("Invalid username or password")</script> <?php
@@ -58,12 +62,31 @@ if (isset($_POST["register"])) {
     if (isset($_POST["password"])) {
         $password = preg_replace("/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)]/", "", $_POST["password"]);
     }
+
     $email = "";
+    if (isset($_POST["email"])) {
+        $email = preg_replace("/[^a-z0-9\-\_\.\@]/", "", $_POST["email"]);
+    }
+
+    if ($username && $password && $email) {
+        try {
+            $pdo = new PDO('sqlite:../db/discordknockoff.sqlite');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $salt = substr(base64_encode(random_bytes(8)), 0, 8);
+
+            $stmt = $pdo->prepare("INSERT INTO users (username, email, password, salt) VALUES (:username, :email, :password, :salt)");
+            $stmt->execute([':username' => $username, ':email' => $email, ':password' => hash('sha256', ($salt.$password)), ':salt' => $salt]);
+
+        } catch (PDOException $e) { // catches any errors within the database manipulation
+            echo "Error: " . $e->getMessage(); // and echos them to the screen
+        }
+    }
 }
 
 if (isset($_POST["logout"])) {
     $_SESSION["loggedIn"] = false;
-    header('location: '.'/');
+    header('location: '.'/login');
 }
 ?>
 
